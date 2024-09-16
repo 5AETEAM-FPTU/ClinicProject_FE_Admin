daef6734fa6294b33c93017475500cad82be48c5
From Vu Vu Developer <vuvo070403@gmail.com>
2024-09-16 15:39:35 +0700

    Update - Contact

--- src/components/Core/modules/Landing/Contact/index.tsx
+++ src/components/Core/modules/Landing/Contact/index.tsx
@@ -1,13 +1,24 @@
-import CommonSection from '@/components/Core/common/CommonSection'
+'use client'
 import React from 'react'
 
+import CommonSection from '@/components/Core/common/CommonSection'
+
+import { useRef } from 'react'
+import { Editor} from '@tinymce/tinymce-react'
+
 function Contact() {
+    const editorRef = useRef<any>(null);
+    const log = () => {
+        if (editorRef.current) {
+            console.log(editorRef.current.getContent())
+        }
+    }
     return (
         <div>
             <CommonSection
                 title={'địa chỉ - thông tin - liên hệ'}
                 subtile={'Khu đô thị FPT City, Ngũ Hành Sơn, Đà Nẵng, Việt Nam'}
-                tailCustomStyle="bg-gradient-to-b from-white to-secondaryLight"
+                tailCustomStyle="bg-gradient-to-b npm install --save @tinymce/tinymce-reactfrom-white to-secondaryLight"
             >
                 <div className="flex flex-col gap-[40px]">
                     <div className="flex flex-row gap-[30px]">
@@ -24,15 +35,86 @@ function Contact() {
                                 ></iframe>
                             </div>
                         </div>
-                        <div className="h-fit w-1/2  flex flex-col gap-[10px]">
+                        <div className="flex h-fit w-1/2 flex-col gap-[10px]">
                             {' '}
-                            <p className="text-secondaryDark text-end">Thời gian hoạt động</p>
-                            <div className="h-[348px] w-full rounded-xl border-2 border-secondaryDark border-dashed bg-white">
-                             
-                            </div>
+                            <p className="text-end text-secondaryDark">
+                                Thời gian hoạt động
+                            </p>
+                            <div className="h-[348px] w-full rounded-xl border-2 border-dashed border-secondaryDark bg-white"></div>
                         </div>
                     </div>
-                    <div></div>
+                    <div className="h-fit w-full">
+                        <Editor
+                            onInit={(_, editor) => {editorRef.current = editor}}
+                            ref={editorRef}
+                            apiKey="kf6v30dw6u6pqnzquj9slxf6eb3a3o1fauu1it6whe4zy9l6"
+                            init={{
+                                height: 200,
+                                statusbar: false,
+                                plugins: [
+                                    // Core editing features
+                                    'anchor',
+                                    'autolink',
+                                    'charmap',
+                                    'codesample',
+                                    'emoticons',
+                                    'image',
+                                    'link',
+                                    'lists',
+                                    'media',
+                                    'searchreplace',
+                                    'table',
+                                    'visualblocks',
+                                    'wordcount',
+                                    // Your account includes a free trial of TinyMCE premium features
+                                    // Try the most popular premium features until Sep 30, 2024:
+                                    'checklist',
+                                    'mediaembed',
+                                    'casechange',
+                                    'export',
+                                    'formatpainter',
+                                    'pageembed',
+                                    'a11ychecker',
+                                    'tinymcespellchecker',
+                                    'permanentpen',
+                                    'powerpaste',
+                                    'advtable',
+                                    'advcode',
+                                    'editimage',
+                                    'advtemplate',
+                                    'ai',
+                                    'mentions',
+                                    'tinycomments',
+                                    'tableofcontents',
+                                    'footnotes',
+                                    'mergetags',
+                                    'autocorrect',
+                                    'typography',
+                                    'inlinecss',
+                                    'markdown',
+                                ],
+                                menubar: false,
+                                toolbar:
+                                    'undo redo | blocks fontfamily fontsize | bold italic underline strikethrough | link image media table mergetags | addcomment showcomments | spellcheckdialog a11ycheck typography | align lineheight | checklist numlist bullist indent outdent | emoticons charmap | removeformat',
+                                tinycomments_mode: 'embedded',
+                                tinycomments_author: 'Author name',
+                                mergetags_list: [
+                                    {
+                                        value: 'First.Name',
+                                        title: 'First Name',
+                                    },
+                                    { value: 'Email', title: 'Email' },
+                                ],
+                                ai_request: (request: any, respondWith: any) =>
+                                    respondWith.string(() =>
+                                        Promise.reject(
+                                            'See docs to implement AI Assistant',
+                                        ),
+                                    ),
+                            }}
+                        ></Editor>
+                        <button onClick={log}>Log editor content</button>
+                    </div>
                 </div>
             </CommonSection>
         </div>
